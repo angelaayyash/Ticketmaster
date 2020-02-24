4704e89cfcff2263b3b185351c5ebe888efc2e28
Angela Ayyash <ayyashangela@gmail.com>
2020-02-24 14:27:45 -0500

    css

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -33,24 +33,4 @@ export class ApiService {
       }
     );
   }
-
-  // getData(
-  //   startDateTime: string = "",
-  //   endDateTime: string = "",
-  //   keyword: string = "",
-  //   city: string = ""
-  // ) {
-  //   return this.http.get(
-  //     "https://app.ticketmaster.com/discovery/v2/events.json?",
-  //     {
-  //       params: {
-  //         apikey: this.API_KEY,
-  //         startDateTime: startDateTime + "T00:00:00Z",
-  //         endDateTime: endDateTime + "T00:00:00Z",
-  //         keyword: keyword,
-  //         city: city
-  //       }
-  //     }
-  //   );
-  // }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { SearchComponent } from "./search/search.component";
 import { EventlistComponent } from "./eventlist/eventlist.component";
+import { HeaderComponent } from "./header/header.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/search", pathMatch: "full" },
   { path: "search", component: SearchComponent },
+  { path: "header", component: HeaderComponent },
   { path: "eventlist", component: EventlistComponent },
   { path: "**", component: SearchComponent }
 ];
